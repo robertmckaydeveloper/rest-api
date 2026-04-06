0d82b35d962b3bd4a3d022aa6adfbd1bf5baaa34
robert <robert.mckay.developer@gmail.com>
2026-04-06 03:23:39 +0000

    resolved mapping and request body issues

---
+++ src/main/java/com/example/restapi/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package com.example.restapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapi.entity.Customer;
+import com.example.restapi.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+}
