70ed9d5122b637df44d9906e9a99c9efb7cb5364
robert <robert.mckay.developer@gmail.com>
2026-04-06 03:24:14 +0000

    removed unused imports

--- src/main/java/com/example/restapi/controller/CustomerController.java
+++ src/main/java/com/example/restapi/controller/CustomerController.java
@@ -2,7 +2,6 @@ package com.example.restapi.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
