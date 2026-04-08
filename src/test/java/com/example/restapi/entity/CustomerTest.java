3f0d42ecc6aff8702263ff38ce70ef01ec11c070
robert <robert.mckay.developer@gmail.com>
2026-04-08 02:51:37 +0000

    added test for customer entity

---
+++ src/test/java/com/example/restapi/entity/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.example.restapi.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+
+@DataJpaTest
+public class CustomerTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCustomerConstructor() {
+
+        var name = "robert";
+        var email = "robert.mckay.developer@gmail.com";
+        Customer customer = new Customer (name, email);
+
+        entityManager.flush();
+
+        assert customer.getName().equals(name);
+        assert customer.getEmail().equals(email);
+    }
+}
